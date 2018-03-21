1476e14c49e647cb1a50dc7e7290d29e9b517f5f
setUserName <466758243@qq.com>
2018-03-21 09:55:31 +0800

    ex.js

---
+++ src/routes/ex.js
@@ -0,0 +1,11 @@
+import React , {Component} from 'react';
+
+class Index extends Component{
+    
+    render(){
+        return(
+            <h3 style={{lineHeight:'50px',textAlign:"center"}}>模块开发中</h3>
+        )
+    }
+}
+export default Index;
