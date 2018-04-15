5a3924ff8ed77c1affe91923fa4c6158a2a852dd
MINZHANG WEI <81228741@qq.com>
2018-04-15 15:24:00 +0800

    需要验证用户的请求

---
+++ src/utils/axiosRequest.js
@@ -0,0 +1,71 @@
+import fetch from 'dva/fetch';
+import axios from 'axios';
+import QS from 'query-string';
+import { Toast } from 'antd-mobile';
+
+import { redirectLogin } from './auth'
+
+function parseJSON(response) {
+  return response.json();
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  if (response && response.status === 401) {
+    redirectLogin();
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
+
+/**
+ * Requests a URL, returning a promise.
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ * @return {object}           An object containing either "data" or "err"
+ */
+export default function request(url, options) {
+
+  let JWT = localStorage.getItem('UT') || '';
+  //return;
+  //请求方式为POST时使用data:{}方式传递参数，使用GET方式时用params:{}方式
+  let methodData = options.method=='get' || "GET" ? {params:options.body}:{data:options.body};
+  //nOptions 存储axios.request(option)发送请求必须的数据option
+  let nOptions = {
+    headers: {
+      'Authorization': 'Bearer ' + JWT.replace(/\"/g,""),
+    },
+    timeout: 5000,
+    validateStatus: function (status) {
+      return (status >= 200 && status < 300)
+    },
+    url:url,
+    ...methodData,
+    ...options
+  }
+
+  return axios.request(nOptions)
+  .then(checkStatus)
+  .then(function(response){
+    //console.log(response);
+    return response.data;
+  })
+  .catch(function(err){
+    //console.log(err.response);
+    const {status, data} = err.response || {};
+    //收集错误信息 通过Toast进行系统提示
+    let errs = '';
+    for(var index in data.errors){
+       errs +=data.errors[index];
+       
+    }
+    Toast.fail(data.msg + errs, 3);
+    //返回错误数据信息
+    return {status, data};
+  });
+}
