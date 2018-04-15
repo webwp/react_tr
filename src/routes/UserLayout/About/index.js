c019ef1dca77122674c36fb34deca36842c34207
MINZHANG WEI <81228741@qq.com>
2018-04-15 11:05:22 +0800

    关于我们

--- src/routes/UserLayout/About/index.js
+++ src/routes/UserLayout/About/index.js
@@ -1,9 +1,16 @@
 import React , { Component } from 'react';
+import Header from '../../../components/Other/Header';
+import Detail from '../../../components/Other/Detail'
 
 class Index extends Component {
     render(){
         return(
-            <div>关于我们</div>
+            <div>
+                <Header {...this.props} headerTxt='关于我们' />
+                <div className="custom-nav-sibling-top">
+                     <Detail />
+                </div>
+            </div>
         )
     }
 }
