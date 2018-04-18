6ac65dc06b351ad0c945a23a0ebe4bd47dc1ad16
MINZHANG WEI <81228741@qq.com>
2018-04-18 17:04:12 +0800

    红包卡券api数据页面

--- src/routes/UserLayout/Coupons/index.js
+++ src/routes/UserLayout/Coupons/index.js
@@ -1,8 +1,10 @@
 import React , { Component } from 'react';
+import { connect } from 'dva';
 import { Tabs, WhiteSpace ,Card, WingBlank,Badge,Button} from 'antd-mobile';
-
-import Header from '../../../components/Other/Header';
-
+import Page from '../../../components/Page'
+@connect( state =>({
+    Coupons:state.coupons
+}))
 class Index extends Component{
     renderContent = tab =>(
         <div style={{  height: '100%' }}>
@@ -16,13 +18,13 @@ class Index extends Component{
                 <dl>
                     <dt>
                         <div>￥<strong className="fz-weak-md">{item.money}</strong></div>
-                        <p className='fz-small-sm' style={{lineHeight: 'normal'}}>{item.tip}</p>
+                        <p className='fz-small-sm' style={{lineHeight: 'normal'}}>{item.title}</p>
                     </dt>
                     <dd>
-                        <div className="fz-small-sm lineH"><Badge text={item.type} hot /><strong>{item.title}</strong></div>
+                        <div className="fz-small-sm lineH"><Badge text={item.category} hot /><strong>{item.title}</strong></div>
                         <p>
-                            <span className="right">{item.status==0?<Button type="ghost" size="small">立即使用</Button>:item.status==1?'未使用':'已过期'}</span>
-                            <span className='fz-small-sm txt-color-default'>{item.recordTime}</span>
+                            <span className="right">{item.used==0?<Button type="ghost" size="small">立即使用</Button>:item.used==1?'未使用':'已过期'}</span>
+                            <span className='fz-small-sm txt-color-default'>有效期：{item.start_time}~{item.end_time}</span>
                         </p>
                     </dd>
                 </dl>
@@ -34,25 +36,33 @@ class Index extends Component{
         </div>
     );
     render(){
-        const tabs = [
-            { title: '未使用' , coupons:[
-                {money:50,type:'满减券',status:'0',title:'水上旅游购满200元减50',tip:'满200减50',recordTime:'2018.06.30~2019.01.30'},
-                {money:30,type:'直减券',status:'0',title:'首绑银联获赠30元直减券',tip:'',recordTime:'2018.06.30~2019.01.30'},
-            ]},
-            { title: '已使用' , coupons:[
-                {money:50,type:'满减券',status:'1',title:'水上旅游购满200元减50',tip:'',recordTime:'2018.06.30~2019.01.30'},
-                {money:30,type:'直减券',status:'1',title:'首绑银联获赠30元直减券',tip:'满200减50',recordTime:'2018.06.30~2019.01.30'},
-            ]},
-            { title: '已过期' , coupons:[
-                {money:50,type:'满减券',status:'2',title:'水上旅游购满200元减50',tip:'满200减50',recordTime:'2018.06.30~2019.01.30'},
-                {money:30,type:'直减券',status:'2',title:'首绑银联获赠30元直减券',tip:'',recordTime:'2018.06.30~2019.01.30'},
-            ]},
+        const { Coupons , history } = this.props;
+        const { readyUsr,donUse,overdue } = Coupons;
+        
+        // const tabs = [
+        //     { title: '未使用' , coupons:[
+        //         {money:50,type:'满减券',status:'0',title:'水上旅游购满200元减50',tip:'满200减50',recordTime:'2018.06.30~2019.01.30'},
+        //         {money:30,type:'直减券',status:'0',title:'首绑银联获赠30元直减券',tip:'',recordTime:'2018.06.30~2019.01.30'},
+        //     ]},
+        //     { title: '已使用' , coupons:[
+        //         {money:50,type:'满减券',status:'1',title:'水上旅游购满200元减50',tip:'',recordTime:'2018.06.30~2019.01.30'},
+        //         {money:30,type:'直减券',status:'1',title:'首绑银联获赠30元直减券',tip:'满200减50',recordTime:'2018.06.30~2019.01.30'},
+        //     ]},
+        //     { title: '已过期' , coupons:[
+        //         {money:50,type:'满减券',status:'2',title:'水上旅游购满200元减50',tip:'满200减50',recordTime:'2018.06.30~2019.01.30'},
+        //         {money:30,type:'直减券',status:'2',title:'首绑银联获赠30元直减券',tip:'',recordTime:'2018.06.30~2019.01.30'},
+        //     ]},
+            
+        //   ];
+          const tabs = [
+            { title: '未使用' , coupons:donUse==null? []:donUse.data},
+            { title: '已使用' , coupons:readyUsr==null ? []:readyUsr.data},
+            { title: '已过期' , coupons:overdue==null ? []:overdue.data},
             
           ];
         return(
-            <div>
-                <Header {...this.props} headerTxt='红包/卡券' />
-                <div className="custom-nav-sibling-top">
+            <Page title="红包/卡券" history={history}>
+                <div className="">
                     {/* <WhiteSpace /> */}
                         <Tabs 
                             tabs={tabs} 
@@ -63,7 +73,7 @@ class Index extends Component{
                         </Tabs>
                     <WhiteSpace />
                 </div>
-            </div>
+            </Page>
             
         )
     }
