b7d6646d275c2c4c1a4f45de093b82e307762a03
MINZHANG WEI <81228741@qq.com>
2018-04-26 15:03:41 +0800

    配置文件修改

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@ const config={
     requestURL:'http://api.traffic.nnpark.cn',
     loginURL  :'#/login?redirect_url=',
     name:'出行南宁',
+    defaultImg:'',
+    handleImageErrored:(e) => {
+        e.target.setAttribute('src','image/defaultImg.png')
+    },
 
 }
 
