dd3ecf407a81c2f325d688919daaef4114284ebb
MINZHANG WEI <81228741@qq.com>
2018-04-26 15:17:44 +0800

    详情页默认图片

--- src/components/Page/detail.js
+++ src/components/Page/detail.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react'
 import { Flex, WhiteSpace } from 'antd-mobile';
 
+import Config from '../../config'
+
 import Header from '../../components/Other/Header'
 
 class Index extends Component{
@@ -11,7 +13,7 @@ class Index extends Component{
                     <h2 className="txt-c mt20">{data.title}</h2>
                     <div className='txt-color-assist-sm fz-mini-sm txt-c box-border-bottom'>{data.publish_time}</div>
                     <div className="txt-color-stage fz-weak-sm box-containt">
-                         <p className="txt-c">{data.image ? <img src={data.image} />: ''}</p>
+                         <p className="txt-c"><img src={data.image} onError={(el)=>Config.handleImageErrored(el)} /></p>
                          {data.content}
                     </div>
                 </div>
