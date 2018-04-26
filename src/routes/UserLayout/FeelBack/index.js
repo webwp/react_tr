6dd8e54b85578d43ddc3982226bebaffdb737372
MINZHANG WEI <81228741@qq.com>
2018-04-26 15:19:14 +0800

    用户反馈

---
+++ src/routes/UserLayout/FeelBack/index.js
@@ -0,0 +1,45 @@
+import React , { Component } from 'react';
+import { List,TextareaItem ,Button,WhiteSpace,Toast} from "antd-mobile";
+import { createForm } from 'rc-form';
+
+import Page from '../../../components/Page';
+
+
+class Index extends Component{
+    onSubmit =(value) => {
+        this.props.form.validateFields((error, value) => {
+            if(!error){
+                console.log(value)
+                Toast.info('提交成功',2)
+            }else{
+                const { count } = error;
+                Toast.info(count.errors[0].message,2)
+            }
+        });
+    }
+    render(){
+        const page = {title:'意见反馈',history:this.props.history};
+        const { getFieldProps } = this.props.form;
+        return(
+            <Page {...page}>
+                <div className='content'>
+                    <List renderHeader={() => '您的反馈是我们前进最大的动力'}>
+                    <TextareaItem
+                        {...getFieldProps('feelback', {rules:[{required:true,message:'反馈的内容不能为空'}]
+                        })}
+                        placeholder="请输入反馈的内容"
+                        rows={5}
+                        count={100}
+                    />
+                    </List>
+                    <WhiteSpace />
+                    <List style={{padding:"12.5px"}}>
+                        <Button type="primary" onClick={this.onSubmit}>提交</Button>
+                    </List>
+                </div>
+            </Page>
+        )
+    }
+}
+const indexForm = createForm()(Index);
+export default indexForm;
