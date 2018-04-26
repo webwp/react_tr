05b1566915f538f779b5301dc925655f34624a3e
MINZHANG WEI <81228741@qq.com>
2018-04-26 15:02:54 +0800

    用户中心bug

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
-import { Link } from 'dva/router'
-import { Button,Result, Icon, WhiteSpace,List,Badge } from 'antd-mobile';
-import { Modal, WingBlank, Toast,Grid } from 'antd-mobile';
+import { Link } from 'dva/router';
+import { Button,Result, Icon, WhiteSpace,List,Badge ,Modal, WingBlank, Toast,Grid} from 'antd-mobile';
 
 const Item = List.Item;
 const myImg = src => <img src={src} style={{'height':'60px','width':'60px'}} className="spe am-icon am-icon-md" alt="" />;
@@ -21,29 +20,44 @@ class Index extends Component{
             payload:''
         })
     }
-    
+    //需要登录
+    authCheck = (url) => {
+        const JWT = localStorage.getItem('UT');
+        const _this = this;
+        if(JWT){
+            this.pageJump(url);
+        }else{
+            Modal.alert('温馨提示','您需要登录用户',[
+                { text:'取消', onPress:()=>{},style:'default'},
+                { text:'去登录', onPress:()=>{_this.pageJump('/login')}}
+            ])
+        }
+    }
+    //页面跳转
+    pageJump = (url)=>{
+        const { history } = this.props;
+        history.push(url);
+    }
     render(){
         const { user } = this.props;
-        console.log( user )
         const { wallet } = user;
-        console.log(wallet)
         const data = [
             {
                 icon:'https://gw.alipayobjects.com/zos/rmsportal/WXoqXTHrSnRcUwEaQgXJ.png',
                 text:'我的二维码',
-                url:'#/user/mycode',
+                url:'/user/mycode',
                 other:''
             },
             {
                 icon:'https://gw.alipayobjects.com/zos/rmsportal/WXoqXTHrSnRcUwEaQgXJ.png',
                 text:'红包/卡券',
-                url:'#/user/coupons',
+                url:'/user/coupons',
                 other:''
             },
             {
                 icon:'',
                 text:'余额',
-                url:'#/user/wallet',
+                url:'/user/wallet',
                 other:wallet == null ? '0.00' : (wallet.data.balance==null? '0.00':wallet.data.balance) 
             }
         ]
@@ -56,7 +70,7 @@ class Index extends Component{
                         <Badge text={10} className='left'>
                             <Link className='left' badge={1} to='/message'><i className="iconfont icon-xiaoxi"></i></Link>
                         </Badge>
-                        <Link className='right' to='/user/set'><i className="iconfont icon-setup"></i></Link>
+                        <span className='right' onClick={()=>{this.authCheck('/user/set')}}><i className="iconfont icon-setup"></i></span>
                     </div>
                     <div className="am-result-pic">
                         <img src="../src/assets/user_1206721_easyicon.net.svg" className="spe am-icon am-icon-md" alt="" style={{height: '60px', width: "60px"}} />
@@ -69,14 +83,14 @@ class Index extends Component{
                     data={data}
                     columnNum='3'
                     hasLine={false}
+                    onClick={_el => this.authCheck(_el.url)}
                     renderItem={dataItem => (
-                        <div style={{ padding: '12.5px' }}>
-                         <a href={dataItem.url}>
+                        <div style={{ padding: '12.5px' }} >
+                         
                           { dataItem.other!=''?<strong style={{ width:'100%',height:'48px','display':'inline-block','lineHeight':'48px','color':'red'}}>{dataItem.other}</strong>:<img src={dataItem.icon} style={{ width: '48px', height: '48px' }} alt="" />}
                           <div style={{ color: '#888', fontSize: '14px', marginTop: '12px' }}>
                             <span>{dataItem.text}</span>
                           </div>
-                          </a>
                         </div>
                       )}
                 />
@@ -85,27 +99,27 @@ class Index extends Component{
                         thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
                         arrow="horizontal"
                         extra="李四"
-                        onClick={() => {}}
+                        onClick={() => this.authCheck('')}
                     >实名认证</Item>
                     <Item
                         thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
                         arrow="horizontal"
-                        onClick={() => {}}
-                    ><a href="#/user/bill/0">账单</a></Item>
+                        onClick={() => this.authCheck('/user/bill') }
+                    >账单</Item>
                     <Item
                         thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
                         arrow="horizontal"
-                        onClick={() => {}}
-                    ><a href="#/user/contactinfo/">常用游客信息</a></Item>
+                        onClick={() => this.authCheck('/user/contactinfo/') }
+                    >常用游客信息</Item>
                     <Item
                         thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
                         arrow="horizontal"
-                        onClick={() => {}}
+                        onClick={() => this.authCheck('')}
                     >银行卡</Item>
                     <Item
                         thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
                         arrow="horizontal"
-                        onClick={() => {}}
+                        onClick={() => this.authCheck('/user/feelback')}
                     >意见反馈</Item>
                     <Item
                         thumb="https://zos.alipayobjects.com/rmsportal/UmbJMbWOejVOpxe.png"
