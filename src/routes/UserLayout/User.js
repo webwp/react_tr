f2bcff199b7f0119eff9db96e89846e1620eb532
MINZHANG WEI <81228741@qq.com>
2018-05-02 16:02:55 +0800

    组件UserHeader

--- src/routes/UserLayout/User.js
+++ src/routes/UserLayout/User.js
@@ -2,14 +2,18 @@ import React,{Component} from "react";
 import {connect} from 'dva';
 import {Redirect} from 'dva/router'
 import User from '../../components/User';
+import UserHeader from './UserHeader';
 
-@connect(state=>({
-    user:state.user
+@connect((state)=>({
+    user:state.user,
 }))
 class Index extends Component{
     render(){
         return(
+        <div className="result-example custom custom-bg">
+           <UserHeader {...this.props} />
            <User {...this.props} /> 
+        </div>
         )
     }
 }
